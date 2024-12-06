23e754c3092674f6663bfe2600c4bb9d85c2068a
dcattaneo <dcattaneo@gmail.com>
2024-12-06 09:43:14 +0100

    finally day 5

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -2,7 +2,7 @@ use std::collections::HashSet;
 
 advent_of_code::solution!(5);
 
-fn solve(input: &str, part_two: bool) -> Option<u32> {
+fn solve_without_indexes(input: &str, part_two: bool) -> Option<u32> {
     let (first_part, second_part) = input.split_once("\n\n").unwrap();
 
     let hash_set: HashSet<(u32, u32)> = first_part
@@ -17,21 +17,19 @@ fn solve(input: &str, part_two: bool) -> Option<u32> {
         second_part
             .lines()
             .filter_map(|s| {
-                println!("{s}");
                 let mut v: Vec<u32> = s
                     .split(',')
                     .map(|num_string| num_string.parse().unwrap())
                     .collect();
 
-                let mut indexes_to_fix: Vec<(usize, usize)> = Vec::new();
+                let mut indexes_to_fix: Vec<(u32, u32)> = Vec::new();
                 for i in 0..v.len() - 1 {
                     for j in i + 1..v.len() {
                         if hash_set.contains(&(v[j], v[i])) {
-                            println!("Problem with : {} {} ({}, {})", v[i], v[j], i, j);
                             if !part_two {
                                 return None;
                             } else {
-                                indexes_to_fix.push((i, j));
+                                indexes_to_fix.push((v[i], v[j]));
                             }
                         }
                     }
@@ -41,43 +39,20 @@ fn solve(input: &str, part_two: bool) -> Option<u32> {
                     if indexes_to_fix.is_empty() {
                         return None;
                     } else {
-                        for k in 0..indexes_to_fix.len() {
-                            let (i, j) = indexes_to_fix[k];
-                            println!("i: {} j: {}", i, j);
-                            let x = v.remove(j);
-                            println!("removed {}", x);
-                            v.insert(i, x);
-                            println!("{:?}", v);
+                        for (first, second) in indexes_to_fix.into_iter() {
+                            //try to move second before first
+                            let second_index = v.iter().position(|x| *x == second).unwrap();
+                            let first_index = v.iter().position(|x| *x == first).unwrap();
 
-                            //shift indexes
-                            for l in k + 1..indexes_to_fix.len() {
-                                println!("{:?}", indexes_to_fix[l]);
-                                //all j become i
-                                if indexes_to_fix[l].0 == j {
-                                    indexes_to_fix[l].0 = i
-                                }
-
-                                if indexes_to_fix[l].1 == j {
-                                    indexes_to_fix[l].1 = i
-                                }
-
-                                //all i must be incremented
-                                if indexes_to_fix[l].0 == i {
-                                    indexes_to_fix[l].0 += 1
-                                }
-
-                                if indexes_to_fix[l].1 == i {
-                                    indexes_to_fix[l].1 += 1
-                                }
-                                println!("{:?}", indexes_to_fix[l]);
+                            if second_index > first_index {
+                                v.remove(second_index);
+                                v.insert(first_index, second);
                             }
-                            println!("{:?}", indexes_to_fix);
                         }
                     }
                 }
 
                 let index_in_the_middle = v.len() / 2;
-                println!("Returning: {}", v[index_in_the_middle]);
                 Some(v[index_in_the_middle])
             })
             .sum::<u32>(),
@@ -85,11 +60,11 @@ fn solve(input: &str, part_two: bool) -> Option<u32> {
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
-    solve(input, false)
+    solve_without_indexes(input, false)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    solve(input, true)
+    solve_without_indexes(input, true)
 }
 
 #[cfg(test)]
@@ -108,3 +83,100 @@ mod tests {
         assert_eq!(result, Some(123));
     }
 }
+
+/* part two doesn't work and it's not faster
+fn solve(input: &str, part_two: bool) -> Option<u32> {
+    let (first_part, second_part) = input.split_once("\n\n").unwrap();
+
+    let hash_set: HashSet<(u32, u32)> = first_part
+        .lines()
+        .map(|s| {
+            let (left_s, right_s) = s.split_once("|").unwrap();
+            (left_s.parse().unwrap(), right_s.parse().unwrap())
+        })
+        .collect();
+
+    Some(
+        second_part
+            .lines()
+            .filter_map(|s| {
+                //println!("{s}");
+                let mut v: Vec<u32> = s
+                    .split(',')
+                    .map(|num_string| num_string.parse().unwrap())
+                    .collect();
+
+                let mut indexes_to_fix: Vec<(usize, usize)> = Vec::new();
+                for i in 0..v.len() - 1 {
+                    for j in i + 1..v.len() {
+                        if hash_set.contains(&(v[j], v[i])) {
+                            //println!("Problem with : {} {} ({}, {})", v[i], v[j], i, j);
+                            if !part_two {
+                                return None;
+                            } else {
+                                indexes_to_fix.push((i, j));
+                            }
+                        }
+                    }
+                }
+
+                //println!("{:?}", indexes_to_fix);
+
+                if part_two {
+                    if indexes_to_fix.is_empty() {
+                        return None;
+                    } else {
+                        for k in 0..indexes_to_fix.len() {
+                            let (i, j) = indexes_to_fix[k];
+
+                            if i < j {
+                                //println!("i: {} j: {}", i, j);
+                                //if i >= v.len() {
+                                //    println!("{:?}", v);
+                                //}
+                                let x = v.remove(j);
+                                //println!("removed {}", x);
+                                //if i > v.len() {
+                                //    println!("i: {} j: {}", i, j);
+                                //    println!("removed {}", x);
+                                //    println!("{:?}", v);
+                                //    println!("k :{}", k);
+                                //}
+                                v.insert(i, x);
+                                //println!("{:?}", v);
+
+                                //shift indexes
+                                for l in k + 1..indexes_to_fix.len() {
+                                    //if l == 29 {
+                                    //    println!("i: {} j: {}", i, j);
+                                    //    println!("{:?}", indexes_to_fix[l]);
+                                    //}
+                                    //all j become i
+                                    //all i must be incremented
+                                    if indexes_to_fix[l].0 == j {
+                                        indexes_to_fix[l].0 = i
+                                    } else if indexes_to_fix[l].0 == i {
+                                        indexes_to_fix[l].0 += 1
+                                    }
+
+                                    if indexes_to_fix[l].1 == j {
+                                        indexes_to_fix[l].1 = i
+                                    } else if indexes_to_fix[l].1 == i {
+                                        indexes_to_fix[l].1 += 1
+                                    }
+                                    //println!("{:?}", indexes_to_fix[l]);
+                                }
+                            }
+                            //println!("{:?}", indexes_to_fix);
+                        }
+                    }
+                }
+
+                let index_in_the_middle = v.len() / 2;
+                //println!("Returning: {}", v[index_in_the_middle]);
+                Some(v[index_in_the_middle])
+            })
+            .sum::<u32>(),
+    )
+}
+    */
