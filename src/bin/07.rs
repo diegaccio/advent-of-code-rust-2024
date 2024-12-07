6f89b092674ed73eeffc18846e4724d5a045f228
dcattaneo <dcattaneo@gmail.com>
2024-12-07 17:42:26 +0100

    fixed test

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -1,3 +1,49 @@
+//! # Bridge Repair
+//!
+//! Great optimization taken from maneatingape
+//!
+//! The equations can be validated using a recursive solution that checks all possible combinations
+//! of operators. However the number of states to check grows exponentially as 2ⁿ in part one
+//! and 3ⁿ in part two.
+//!
+//! As much faster approach works in reverse from the end of the equation to prune as many states
+//! as possible by checking which operations are possible. For example:
+//!
+//! ```none
+//!     Test Value: 123456
+//!     Equation: 2 617 56
+//!     Addition is possible as 123456 >= 56
+//!     Multiplication is not possible as 56 is not a factor of 123456
+//!     Concatenation is possible as 1234 || 56 = 123456
+//! ```
+//!
+//! Following the concatenation branch and applying an inverse concentation
+//! (the full solution would also follow the addition branch):
+//!
+//! ```none
+//!     Test Value: 1234
+//!     Equation: 2 617
+//!     Addition is possible as 1234 >= 617
+//!     Multiplication is possible as 2 * 617 = 1234
+//!     Concatenation is not possible as 1234 does not end in 617
+//! ```
+//!
+//! Following the multiplication branch:
+//!
+//! ```none
+//!     Test Value: 2
+//!     Equation: 2
+//! ```
+//!
+//! The test value is equal to the last term which means that the equation is valid.
+//!
+//! Inverse concenation can be implemented without time consuming conversion to or from
+//! strings by dividing the left term by the next power of ten greater than the right term.
+//! For example:
+//!
+//! * 7 || 9 => 79 => 79 / 10 => 7
+//! * 12 || 34 => 1234 => 1234 / 100 => 12
+//! * 123 || 789 => 123789 => 123789 / 1000 => 123
 advent_of_code::solution!(7);
 
 use rayon::prelude::*;
@@ -15,7 +61,6 @@ fn parse_and_solve(input: &str, second_part: bool) -> Option<u64> {
                     .split_whitespace()
                     .map(|num_str| num_str.parse().unwrap())
                     .collect();
-                //if solve(target, &values, values[0], 1, false) {
                 if backward_solve(values[0], &values, target, values.len() - 1, false) {
                     Some(target)
                 } else if second_part {
@@ -140,6 +185,6 @@ mod tests {
     }
     #[test]
     fn test_concat() {
-        assert_eq!(concat_values(3, 12), 312);
+        assert_eq!(_concat_values(3, 12), 312);
     }
 }
